9c5f2c1cc49978590bbe41235ee459458a3d68e2
robcrvg <robjr2013@gmail.com>
2020-08-12 06:34:10 -0300

    Commit inittial

--- app/src/main/java/br/com/provaandroid/MainActivity.java
+++ app/src/main/java/br/com/provaandroid/MainActivity.java
@@ -2,13 +2,33 @@ package br.com.provaandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ImageView nuvemOn, nuvemOff;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private void verificaConexao() {
+        boolean on = true;
+        boolean off = false;
+        ConnectivityManager cm =
+                (ConnectivityManager) getApplicationContext().getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        boolean isConnected = activeNetwork != null &&
+                activeNetwork.isConnectedOrConnecting();
+        if (isConnected) {
+
+        }
     }
 }
